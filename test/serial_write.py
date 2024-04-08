08da5323a3fa25b1d7cb735be3d6fb8a816dabd1
Jason W. DeGraw <jason.degraw@gmail.com>
2024-04-08 07:07:26 -0400

    Add testing

--- test/serial_write.py
+++ test/serial_write.py
@@ -52,3 +52,89 @@ def test_exodusii_write_2(tmpdir):
 
             values = exof.get_times()
             assert np.allclose(values, times)
+
+
+def test_exodusii_write_3(tmpdir):
+    # this test sets more values then reads back those values
+    ndim = 2
+    type = 'QUAD'
+    node_count = 4
+    cell_count = 1
+    x = np.array([0.0, 1.0, 0.0, 1.0])
+    y = np.array([0.0, 0.0, 1.0, 1.0])
+    conn = np.array([[0, 1, 2, 3]])
+    block_names = ['cell']
+    sideset_names = ['sideset_1', 'sideset_2']
+    sideset_cells = [[0], [0, 0]]
+    sideset_sides = [[1], [2, 3]]
+    with util.working_dir(tmpdir.strpath):
+        with exodusii.File('write_3.exo', mode="w") as exof: 
+            exof.put_init('Write_3', ndim, node_count, cell_count,
+                          len(block_names), 0, len(sideset_names))
+            exof.put_coord(x, y)
+            exof.put_element_block(1, type, cell_count, node_count)
+            exof.put_element_block_name(1, 'block_1')
+            exof.put_element_conn(1, conn)
+            # Side set 1 
+            exof.put_side_set_param(1, len(sideset_cells[0]))
+            exof.put_side_set_name(1, sideset_names[0])
+            exof.put_side_set_sides(1, sideset_cells[0], sideset_sides[0])
+            # Side set 2
+            exof.put_side_set_param(2, len(sideset_cells[1]))
+            exof.put_side_set_name(2, sideset_names[1])
+            exof.put_side_set_sides(2, sideset_cells[1], sideset_sides[1])
+        with exodusii.File('write_3.exo', mode="r") as exof:
+            assert exof.title() == "Write_3"
+            assert exof.num_dimensions() == 2
+            assert exof.num_nodes() == 4
+            assert exof.num_elems() == 1
+            assert exof.num_blks() == 1
+            assert exof.num_node_sets() == 0
+            assert exof.num_side_sets() == 2
+            coords = exof.get_coords()
+            assert coords.shape == (4,2)
+            xf = coords[:,0]
+            assert np.allclose(x, xf)
+            yf = coords[:,1]
+            assert np.allclose(y, yf)
+            blk = exof.get_element_block(1)
+            assert blk.name == 'block_1'
+            ss = exof.get_side_set(1)
+            assert ss.name == 'sideset_1'
+            ss = exof.get_side_set(2)
+            assert ss.name == 'sideset_2'
+
+
+def test_exodusii_write_4(tmpdir):
+    # this test sets multiple block names at once then reads back those values
+    ndim = 2
+    type = 'QUAD'
+    node_count = 4
+    cell_count = 1
+    x = np.array([0.0, 1.0, 0.0, 1.0])
+    y = np.array([0.0, 0.0, 1.0, 1.0])
+    block_names = ['block1', 'block2', 'block3']
+    with util.working_dir(tmpdir.strpath):
+        with exodusii.File('write_4.exo', mode="w") as exof: 
+            exof.put_init('Write_4', ndim, node_count, cell_count,
+                          len(block_names), 0, 0)
+            exof.put_coord(x, y)
+            exof.put_element_block(1, type, cell_count, node_count)
+            exof.put_element_block(2, type, cell_count, node_count)
+            exof.put_element_block(3, type, cell_count, node_count)
+            exof.put_element_block_names(block_names)
+        with exodusii.File('write_4.exo', mode="r") as exof:
+            assert exof.title() == "Write_4"
+            assert exof.num_dimensions() == 2
+            assert exof.num_nodes() == 4
+            assert exof.num_elems() == 1
+            assert exof.num_blks() == 3
+            assert exof.num_node_sets() == 0
+            assert exof.num_side_sets() == 0
+            blk = exof.get_element_block(1)
+            assert blk.name == block_names[0]
+            blk = exof.get_element_block(2)
+            assert blk.name == block_names[1]
+            blk = exof.get_element_block(3)
+            assert blk.name == block_names[2]
+
