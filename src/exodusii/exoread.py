095a883880277199e80891d411a5bdeaf0416e05
Tim Fuller <tjfulle@sandia.gov>
2026-09-07 23:05:12 -0600

    Update exoread.py

--- src/exodusii/exoread.py
+++ src/exodusii/exoread.py
@@ -4,9 +4,8 @@
 
 """Legacy exoread module."""
 
-from exodusii.cli.exoread import Namespace
-from exodusii.cli.exoread import build_parser
+
 from exodusii.cli.exoread import describe
 from exodusii.cli.exoread import main
 
-__all__ = ["Namespace", "build_parser", "describe", "main"]
+__all__ = ["describe", "main"]
